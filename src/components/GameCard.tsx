947292ce99d346c7f2c2ad78747d1d936a4236c5
saikonuru <sairam.konuru@gmail.com>
2025-01-27 22:33:49 +0530

    Add GameCard component

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,21 @@
+import { Card, CardBody } from "@chakra-ui/card";
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Heading, Image } from "@chakra-ui/react";
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image}></Image>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
