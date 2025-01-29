b6816dd87b6d19d9a8167e27145d163ea8e92be2
saikonuru <sairam.konuru@gmail.com>
2025-01-29 17:01:43 +0530

    Add Platform Icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,21 +1,24 @@
 import { Card, CardBody } from "@chakra-ui/card";
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Heading, Image } from "@chakra-ui/react";
+import { Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
 }
 
-const GameCard = ({ game }: Props) => {
-  return (
-    <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image}></Image>
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-      </CardBody>
-    </Card>
-  );
-};
+const GameCard = ({ game }: Props) => (
+  <Card borderRadius={10} overflow="hidden">
+    <Image src={game.background_image}></Image>
+    <CardBody>
+      <Heading fontSize="2xl">{game.name}</Heading>
+
+      <PlatformIconList
+        platforms={game.parent_platforms.map((p) => p.platform)}
+      ></PlatformIconList>
+    </CardBody>
+  </Card>
+);
 
 export default GameCard;
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,45 @@
+import {
+  FaWindows,
+  FaLinux,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+} from "react-icons/fa";
+
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+
+import GameCard from "./GameCard";
+import { Text, Icon, HStack } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  // Index signature
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    android: FaAndroid,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform: Platform) => (
+        // <Text key={platform.id}> {platform.name} </Text>
+        <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
