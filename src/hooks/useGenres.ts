5aecec41480ed15938ab716d08be98e1487e7e9a
saikonuru <sairam.konuru@gmail.com>
2025-03-13 23:05:56 +0530

    Refactor GenreList to use results from data and update useGenres hook to use static genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,5 @@
-import useData from './useData';
-
+// import useData from './useData';
+import genres from '../data/genres';
 export interface Platform {
   id: number;
   name: string;
@@ -12,5 +12,6 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => useData<Genre>('/genres', {}, []);
+// const useGenres = () => useData<Genre>('/genres', {}, []);
+const useGenres = () => ({data: genres, isLoading: false, error: null});
 export default useGenres;
