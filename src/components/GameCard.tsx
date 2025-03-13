bc0c0638f377336323f914ff5ccb974ebafa6b7f
saikonuru <sairam.konuru@gmail.com>
2025-03-13 22:02:11 +0530

    Cleanup the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
-import { Card, CardBody } from '@chakra-ui/card';
-import { Game } from '../hooks/useGames';
-import { Heading, HStack, Image } from '@chakra-ui/react';
+import {Card, CardBody} from '@chakra-ui/card';
+import {Game} from '../hooks/useGames';
+import {Heading, HStack, Image} from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 
@@ -8,18 +8,17 @@ interface Props {
   game: Game;
 }
 
-const GameCard = ({ game }: Props) => (
+const GameCard = ({game}: Props) => (
   <Card>
-    <Image src={game.background_image}></Image>
+    <Image src={game.background_image}> </Image>
+
     <CardBody>
-      <Heading fontSize="2xl">{game.name}</Heading>
-
-      <HStack justifyContent="space-between">
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}></PlatformIconList>
-
+      <HStack justifyContent="space-between" marginBottom={3}>
+        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
         <CriticScore score={game.metacritic} />
       </HStack>
+
+      <Heading fontSize="2xl">{game.name}</Heading>
     </CardBody>
   </Card>
 );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,30 +1,30 @@
-import { SimpleGrid, Text } from '@chakra-ui/react';
+import {SimpleGrid, Text} from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
-import { GameQuery } from '../App';
+import {GameQuery} from '../App';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
-const GameGrid = ({ gameQuery }: Props) => {
-  const { error, data, isLoading } = useGames(gameQuery);
+const GameGrid = ({gameQuery}: Props) => {
+  const {error, data, isLoading} = useGames(gameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
       {error && <Text> {error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding="10px" spacing={3}>
+      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding="10px" spacing={6}>
         {isLoading &&
-          Skeletons.map((Skeleton) => (
+          Skeletons.map(Skeleton => (
             <GameCardContainer key={Skeleton}>
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
 
-        {data.map((game) => (
+        {data.map(game => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
