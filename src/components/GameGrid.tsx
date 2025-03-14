b49d5177ddb6a50307c79e2b34279a97ae8a8760
saikonuru <sairam.konuru@gmail.com>
2025-03-14 10:40:44 +0530

    Change page title.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = ({gameQuery}: Props) => {
   const {error, data, isLoading} = useGames(gameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (!error) return <Text> {error} </Text>;
+  if (error) return <Text> {error} </Text>;
 
   return (
     <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding="10px" spacing={6}>
