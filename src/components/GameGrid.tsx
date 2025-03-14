85f380a1cedacbfdd0cbc6995a176a9b6145f88f
saikonuru <sairam.konuru@gmail.com>
2025-03-14 10:12:29 +0530

    Refractor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,24 +13,23 @@ const GameGrid = ({gameQuery}: Props) => {
   const {error, data, isLoading} = useGames(gameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6];
 
-  return (
-    <>
-      {error && <Text> {error}</Text>}
-      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding="10px" spacing={6}>
-        {isLoading &&
-          Skeletons.map(Skeleton => (
-            <GameCardContainer key={Skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
+  if (!error) return <Text> {error} </Text>;
 
-        {data.map(game => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+  return (
+    <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding="10px" spacing={6}>
+      {isLoading &&
+        Skeletons.map(Skeleton => (
+          <GameCardContainer key={Skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+
+      {data.map(game => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
